f3d94f98909bdb29395e375beb2220bd8dfdb2fc
mooziii <mooz@moozunity.de>
2021-03-15 22:13:48 +0100

    Update Manager.kt

--- src/main/kotlin/de/hglabor/Manager.kt
+++ src/main/kotlin/de/hglabor/Manager.kt
@@ -1,24 +1,16 @@
 package de.hglabor
 
-import com.gitlab.kordlib.kordx.emoji.Emojis
 import de.hglabor.command.CommandManager
 import de.hglabor.config.ConfigManager
-import dev.kord.common.Color
 import dev.kord.common.annotation.KordPreview
-import dev.kord.common.entity.Permission
-import dev.kord.common.entity.Snowflake
 import dev.kord.core.Kord
-import dev.kord.core.behavior.channel.MessageChannelBehavior
-import dev.kord.core.behavior.channel.createEmbed
 import dev.kord.core.entity.Guild
 import dev.kord.core.entity.Member
-import dev.kord.core.entity.ReactionEmoji
 import dev.kord.core.entity.interaction.Interaction
-import dev.kord.core.event.message.MessageCreateEvent
-import dev.kord.core.event.message.ReactionAddEvent
+import dev.kord.core.event.gateway.DisconnectEvent
+import dev.kord.core.event.gateway.ReadyEvent
+import dev.kord.core.event.guild.GuildCreateEvent
 import dev.kord.core.on
-import dev.kord.rest.Image
-import dev.kord.rest.builder.message.EmbedBuilder
 import kotlinx.coroutines.flow.collect
 
 @KordPreview
@@ -37,6 +29,23 @@ object Manager {
             ConfigManager.discordApplication.token
                 ?: error("No token provided"))
         CommandManager.init()
+        client.on<ReadyEvent> {
+            client.guilds.collect {
+                if(it.systemChannel != null) {
+                    it.systemChannel!!.createMessage("Bot started successfully")
+                }
+            }
+        }
+        client.on<DisconnectEvent> {
+            client.guilds.collect {
+                if(it.systemChannel != null) {
+                    it.systemChannel!!.createMessage("Bot got disconnected from this guild.")
+                }
+            }
+        }
+        client.on<GuildCreateEvent> {
+            print("Connected to guild ${this.guild.name}")
+        }
         client.login()
     }
 }
