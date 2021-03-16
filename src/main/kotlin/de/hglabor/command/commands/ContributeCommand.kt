62c0b38ef4554fd26824cdefeb236fab864b8cba
mooziii <mooz@moozunity.de>
2021-03-16 10:18:43 +0100

    Create ContributeCommand.kt

---
+++ src/main/kotlin/de/hglabor/command/commands/ContributeCommand.kt
@@ -0,0 +1,31 @@
+package de.hglabor.command.commands
+
+import de.hglabor.command.SlashCommand
+import dev.kord.common.Color
+import dev.kord.common.annotation.KordPreview
+import dev.kord.core.behavior.followUp
+import dev.kord.core.entity.interaction.Interaction
+import dev.kord.rest.builder.message.EmbedBuilder
+
+@KordPreview
+object ContributeCommand : SlashCommand(
+    "contribute",
+    "Erhalte den Link zur GitHub Repository vom Bot"
+) {
+    override suspend fun handleCommand(interaction: Interaction) {
+        interaction.acknowledge().followUp {
+            embed {
+                title = "GitHub Repository"
+                description = "Klicke [GitHub]Hier(https://github.com/mooziii/HGLaborSurvivalBot) um zur GitHub Repository des Bot zu gelangen."
+                val thumb = EmbedBuilder.Thumbnail()
+                thumb.url = interaction.kord.getSelf().avatar.url
+                thumbnail = thumb
+                val foot = EmbedBuilder.Footer()
+                foot.icon = interaction.kord.getSelf().avatar.url
+                foot.text = "HGLaborSurvival Bot"
+                footer = foot
+                color = Color(0, 251, 255)
+            }
+        }
+    }
+}
