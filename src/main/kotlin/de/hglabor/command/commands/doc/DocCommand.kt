745f89e15b4389b49d04e813435211ba82131a1e
mooziii <mooz@moozunity.de>
2021-03-16 09:39:47 +0100

    Create DocCommand

--- src/main/kotlin/de/hglabor/command/commands/doc/DocCommand.kt
+++ src/main/kotlin/de/hglabor/command/commands/doc/DocCommand.kt
@@ -1,19 +1,60 @@
 package de.hglabor.command.commands.doc
 
 import de.hglabor.command.SlashCommand
+import de.hglabor.command.commands.doc.entries.SpawnEntry
+import dev.kord.common.Color
 import dev.kord.common.annotation.KordPreview
+import dev.kord.core.behavior.followUp
 import dev.kord.core.entity.interaction.Interaction
+import dev.kord.core.entity.interaction.string
+import dev.kord.rest.builder.message.EmbedBuilder
+
+val docEntries = listOf<DocEntry>(SpawnEntry)
 
 @KordPreview
 object DocCommand : SlashCommand(
     name = "docs",
-    "Show information about everything in the game."
+    "Show information about everything in the game.",
+    {
+        subCommand("read", "Zeigt Informationen über bestimmte Dinge") {
+            string("entry", "Der Dokumentationseintrag der angezeigt werden soll") {
+                required = true
+                for (it in docEntries)
+                    choice(it.name, it.name)
+            }
+        }
+    }
 ) {
 
-    val docEntries = listOf<DocEntry>()
+
 
     override suspend fun handleCommand(interaction: Interaction) {
-        
+        for(docEntry in docEntries) {
+            val entry = interaction.command.options["entry"]?.string()
+            if (entry != null) {
+                if(docEntry.keywords.contains(entry.toLowerCase())) {
+                    interaction.acknowledge().followUp {
+                        embed {
+                            title = "Verfasst von ${docEntry.author}"
+                            description = docEntry.description
+                            val thumb = EmbedBuilder.Thumbnail()
+                            thumb.url = interaction.kord.getSelf().avatar.url
+                            thumbnail = thumb
+                            val foot = EmbedBuilder.Footer()
+                            foot.icon = interaction.kord.getSelf().avatar.url
+                            foot.text = "HGLaborSurvival Bot"
+                            footer = foot
+                            color = Color(0, 251, 255)
+                            image = docEntry.image
+                            field {
+                                name = docEntry.name
+                                value = docEntry.text
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
 }
