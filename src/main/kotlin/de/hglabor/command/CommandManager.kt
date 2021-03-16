5572e84ccea1e8ab0b811f09bda61ca3b45e5f5e
mooziii <mooz@moozunity.de>
2021-03-16 09:39:56 +0100

    Register DocCommand

--- src/main/kotlin/de/hglabor/command/CommandManager.kt
+++ src/main/kotlin/de/hglabor/command/CommandManager.kt
@@ -2,6 +2,7 @@ package de.hglabor.command
 
 import de.hglabor.Manager
 import de.hglabor.command.commands.HelpCommand
+import de.hglabor.command.commands.doc.DocCommand
 import dev.kord.common.annotation.KordPreview
 import dev.kord.core.behavior.createApplicationCommand
 import dev.kord.core.entity.Guild
@@ -21,6 +22,7 @@ object CommandManager {
 
     suspend fun init() {
         HelpCommand
+        DocCommand
         cleanupGuilds()
         Manager.client.guilds.collect {
             println("Registering commands for ${it.name}")
