090863e4137dff84fe3c7e030e3020f77bbda06e
mooziii <63669478+mooziii@users.noreply.github.com>
2021-03-16 14:32:42 +0100

    Update HeadEntry.kt

--- src/main/kotlin/de/hglabor/command/commands/doc/entries/HeadEntry.kt
+++ src/main/kotlin/de/hglabor/command/commands/doc/entries/HeadEntry.kt
@@ -4,7 +4,7 @@ import de.hglabor.command.commands.doc.DocEntry
 
 object HeadEntry : DocEntry(
     "HeadDrops",
-    "Zu einer gewissen wahrscheinlichkeit können Mobs und Tiere in Survival ihren Kopf droppen. Looting beeinflusst diese wahrscheinlichkeit nicht. Die Funktion dass Spieler ihren Kopf droppen wurde nach dem MapReset entfernt. Wenn ein Monster \"NQRMAN\" heißt, droppt es nicht seinen Kopf, sondern einen Garfield Kopf",
+    "Zu einer gewissen wahrscheinlichkeit können Mobs und Tiere in Survival ihren Kopf droppen. Looting beeinflusst diese wahrscheinlichkeit nicht. Die Funktion dass Spieler ihren Kopf droppen wird nach dem 1.17-MapReset entfernt. Wenn ein Monster \"NQRMAN\" heißt, droppt es nicht seinen Kopf, sondern einen Garfield Kopf",
     "Informationen zu den HeadDrops auf Survival",
     "https://i.imgur.com/vcwsSm8.gif",
     "MooZiii",
