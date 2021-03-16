de8f9cea6b04d9e6a6ec9653c5915844e3484542
mooziii <mooz@moozunity.de>
2021-03-16 09:39:38 +0100

    Create SpawnEntry.kt

---
+++ src/main/kotlin/de/hglabor/command/commands/doc/entries/SpawnEntry.kt
@@ -0,0 +1,12 @@
+package de.hglabor.command.commands.doc.entries
+
+import de.hglabor.command.commands.doc.DocEntry
+
+object SpawnEntry : DocEntry(
+    "Spawn",
+    "Dies ist der Ort an dem du Spawnst wenn du den Server das erste mal betrittst.",
+    "Informationen über den Spawn von HGLaborSurvival",
+    "https://cdn.discordapp.com/attachments/739437432197349437/821297422046068746/unknown.png",
+    "MooZiii",
+    arrayListOf("spawn")
+)
