e5357785607cce07f4171544e3545a4b424df7d8
mooziii <mooz@moozunity.de>
2021-03-18 10:22:22 +0100

    buggy?

--- src/main/kotlin/de/hglabor/command/commands/doc/entries/LootableBodiesEntry.kt
+++ src/main/kotlin/de/hglabor/command/commands/doc/entries/LootableBodiesEntry.kt
@@ -3,7 +3,7 @@ package de.hglabor.command.commands.doc.entries
 import de.hglabor.command.commands.doc.DocEntry
 
 object LootableBodiesEntry : DocEntry(
-    "Lootable Bodies",
+    "LootableBodies",
     "Am 18.03.2021 wurde auf HGLaborSurvival die Funktion, dass LootableBodies auch Leichen genannt spawnen, sobald ein Spieler stirbt. Klickt man diese Leiche an, droppen die Items welche der Besitzer dabei hatte. XP-Level sind davon nicht betroffen. Die Leichen kann man nur mit einer Schaufel wieder beseitigen. Die Idee dieses Features stammt aus der originalen 'LootableBodies' Mod: https://www.curseforge.com/minecraft/mc-mods/drcyanos-lootable-bodies Das Plugin ist auch opensource auf Github verfügbar: https://github.com/mooziii/LootableBodies",
     "Erhalte Informationen zur LootableBodies Funktion",
     "https://cdn.discordapp.com/attachments/804656969281175602/822035516261138482/unknown.png",
