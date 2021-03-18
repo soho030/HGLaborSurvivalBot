0c65cee674ab480369fcf55e9436b35fdc0fd9c5
mooziii <mooz@moozunity.de>
2021-03-18 11:55:25 +0100

    Add missing backslashes

--- src/main/kotlin/de/hglabor/command/commands/doc/entries/LootableBodiesEntry.kt
+++ src/main/kotlin/de/hglabor/command/commands/doc/entries/LootableBodiesEntry.kt
@@ -4,7 +4,7 @@ import de.hglabor.command.commands.doc.DocEntry
 
 object LootableBodiesEntry : DocEntry(
     "LootableBodies",
-    "Am 18.03.2021 wurde die Funktion "Lootable Bodies" hinzugefügt. Dabei handelt es sich ursprünglich um eine Mod bei der gestorbene Spieler eine Leiche zurücklassen. Anstatt, dass alle Items fallen gelassen werden, werden sie im Inventar der Leiche gespeichert, sodass man sie später durch anklicken der Leiche plündern kann (XP-Level ausgeschlossen). Leichen können daraufhin mit einer Schaufel beseitigt werden. Link zur Mod: https://www.curseforge.com/minecraft/mc-mods/drcyanos-lootable-bodies Link zum Plugin: https://github.com/mooziii/LootableBodies",
+    "Am 18.03.2021 wurde die Funktion \"Lootable Bodies\" hinzugefügt. Dabei handelt es sich ursprünglich um eine Mod bei der gestorbene Spieler eine Leiche zurücklassen. Anstatt, dass alle Items fallen gelassen werden, werden sie im Inventar der Leiche gespeichert, sodass man sie später durch anklicken der Leiche plündern kann (XP-Level ausgeschlossen). Leichen können daraufhin mit einer Schaufel beseitigt werden. Link zur Mod: https://www.curseforge.com/minecraft/mc-mods/drcyanos-lootable-bodies Link zum Plugin: https://github.com/mooziii/LootableBodies",
     "Erhalte Informationen zur LootableBodies Funktion",
     "https://cdn.discordapp.com/attachments/804656969281175602/822035516261138482/unknown.png",
     "MooZiii",
