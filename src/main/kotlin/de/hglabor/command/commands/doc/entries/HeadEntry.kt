ec34aeb2234220b79eb1d9093aa1f2c3ec525de2
mooziii <mooz@moozunity.de>
2021-03-28 10:09:32 +0200

    Fix authors

--- src/main/kotlin/de/hglabor/command/commands/doc/entries/HeadEntry.kt
+++ src/main/kotlin/de/hglabor/command/commands/doc/entries/HeadEntry.kt
@@ -7,6 +7,6 @@ object HeadEntry : DocEntry(
     "Mobs können zu gewisser Wahrscheinlichkeit ihre Köpfe droppen. Looting beeinflusst die Drop-Rate dabei nicht. Wenn ein Mob \"NQRMAN\" heißt, droppt es nicht seinen Kopf, sondern einen Garfield Kopf. Diese Funktion wird in Season 2 (1.17 Patch) nicht mehr unterstützt.",
     "Informationen zu den HeadDrops auf Survival",
     "https://i.imgur.com/vcwsSm8.gif",
-    "MooZiii",
+    "MooZiii, DrSoho",
     arrayListOf("heads", "headdrops")
 )
