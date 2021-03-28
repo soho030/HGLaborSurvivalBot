0065c298a05a724c35983f6cf08acdc71e9e5e5f
mooziii <63669478+mooziii@users.noreply.github.com>
2021-03-28 14:09:32 +0200

    Update GravestoneEntry.kt

--- src/main/kotlin/de/hglabor/command/commands/doc/entries/GravestoneEntry.kt
+++ src/main/kotlin/de/hglabor/command/commands/doc/entries/GravestoneEntry.kt
@@ -7,6 +7,6 @@ object GravestoneEntry : DocEntry(
     "Am 27.03.2021 wurden Leichen durch Grabsteine ersetzt. Sollte ein Spieler sterben, wird an seiner Todesstelle ein Schild platziert. Dieses enthält Informationen über dein Ableben (u. a. Datum & Uhrzeit), sowie die ID. Diese ist jedoch für dich irrelevant, da sie von uns benutzt wird, um die fallengelassenen Items zu speichern. Wird das Schild nun abgebaut werden die Items, die der Spieler bei sich hatte, fallengelassen.",
     "Am 27.03.2021 wurden Leichen durch Grabsteine ersetzt...",
     "https://cdn.discordapp.com/attachments/804656969281175602/825636706874556436/unknown.png",
-    "MooZiii",
+    "MooZiii, DrSoho",
     arrayListOf("gravestones", "gravestone", "grabstein", "grabsteine")
 )
