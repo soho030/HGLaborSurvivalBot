66906b3fa62713ec4f924206133a52b6f0b3af66
mooziii <mooz@moozunity.de>
2021-03-28 10:14:20 +0200

    Fix names

--- src/main/kotlin/de/hglabor/command/commands/doc/entries/ColorCommandEntry.kt
+++ src/main/kotlin/de/hglabor/command/commands/doc/entries/ColorCommandEntry.kt
@@ -3,7 +3,7 @@ package de.hglabor.command.commands.doc.entries
 import de.hglabor.command.commands.doc.DocEntry
 
 object ColorCommandEntry : DocEntry(
-    "/color-Command",
+    "ColorCommand",
     "Der `/color` Command erlaubt es Spielern ihre Schriftfarbe im Chat zu ändern. Die Syntax ist `/color <color>`.",
     "Der `/color` Command erlaubt es Spielern ihre Schriftfarbe zu ändern...",
     "https://i.imgur.com/Zm01NI0.png",
--- src/main/kotlin/de/hglabor/command/commands/doc/entries/CombatLogEntry.kt
+++ src/main/kotlin/de/hglabor/command/commands/doc/entries/CombatLogEntry.kt
@@ -3,7 +3,7 @@ package de.hglabor.command.commands.doc.entries
 import de.hglabor.command.commands.doc.DocEntry
 
 object CombatLogEntry : DocEntry(
-    "Anti-CombatLogging",
+    "CombatLogging",
     "Dieses Feature verbietet es, sich im Fight auszuloggen. Sobald du einen Spieler schlägst seid ihr beide in einer 5 sekündigen Kampfphase. Sollte sich einer von euch beiden während dieser Kampfphase ausloggen, stirbt er.",
     "Dieses Feature verbietet es, sich im Fight auszuloggen.",
     "https://i.imgur.com/ldTLPzH.png",
