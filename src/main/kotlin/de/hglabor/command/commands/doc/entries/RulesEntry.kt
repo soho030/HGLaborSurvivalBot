3a0678248bebec7d3b7ae996fb01fb8168593d4f
mooziii <mooz@moozunity.de>
2021-03-28 10:09:43 +0200

    Change rules

--- src/main/kotlin/de/hglabor/command/commands/doc/entries/RulesEntry.kt
+++ src/main/kotlin/de/hglabor/command/commands/doc/entries/RulesEntry.kt
@@ -4,7 +4,7 @@ import de.hglabor.command.commands.doc.DocEntry
 
 object RulesEntry : DocEntry(
     "Regeln",
-    "Die Regeln von Survival wurden nach dem MapReset deutlich überarbeitet. Die neuen Regeln bestehen aus: \n`1) Grief at your own risk`\n`2) No X-Ray / Hacking`\n`3) Fighting is allowed`. \n Das Team muss sich zu keinem Zeitpunkt für eine Strafe rechtfertigen und die Ausrede \"Das wusste ich nicht\" zählt nicht.",
+    "Die Regeln von Survival wurden nach dem MapReset deutlich überarbeitet. Die neuen Regeln bestehen aus: \n`1) Grief at your own risk`\n`2) No X-Ray`\n`3) Fighting is allowed`. \n Das Team muss sich zu keinem Zeitpunkt für eine Strafe rechtfertigen und die Ausrede \"Das wusste ich nicht\" zählt nicht.",
     "Zeigt die Regeln vom Survival Server an",
     "https://i.imgur.com/nqsYKgv.png",
     "MooZiii",
