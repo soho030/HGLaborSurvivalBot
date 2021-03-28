f9f91641eb3fbf383380e11214ba58f5081292ab
mooziii <mooz@moozunity.de>
2021-03-28 10:09:25 +0200

    Create GravestoneEntry.kt

---
+++ src/main/kotlin/de/hglabor/command/commands/doc/entries/GravestoneEntry.kt
@@ -0,0 +1,12 @@
+package de.hglabor.command.commands.doc.entries
+
+import de.hglabor.command.commands.doc.DocEntry
+
+object GravestoneEntry : DocEntry(
+    "Gravestones",
+    "Am 27.03.2021 wurde die Leichen funktion durch diese Grabstein funktion ausgetauscht. Sollte ein Spieler sterben, wird an seiner Todesstelle ein Schild platziert welches Informationen über den Tod zeigt und zuletzt die ID. Die ID ist nicht wichtig für dich, sondern nur dafür da die Items auf den Block zu binden. Sollte jemand dieses Schild abbauen, dropped der Stuff, des toten.",
+    "Am 27.03.2021 wurde die Leichen funktion durch eine Grabstein funktion ausgetauscht...",
+    "https://cdn.discordapp.com/attachments/804656969281175602/825636706874556436/unknown.png",
+    "MooZiii",
+    arrayListOf("gravestones", "gravestone", "grabstein", "grabsteine")
+)
