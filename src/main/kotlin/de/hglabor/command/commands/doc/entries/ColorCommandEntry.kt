9c3b65cd45d3c98a0526207eec156f78e0e66b01
mooziii <mooz@moozunity.de>
2021-03-28 10:09:18 +0200

    Create ColorCommandEntry.kt

---
+++ src/main/kotlin/de/hglabor/command/commands/doc/entries/ColorCommandEntry.kt
@@ -0,0 +1,12 @@
+package de.hglabor.command.commands.doc.entries
+
+import de.hglabor.command.commands.doc.DocEntry
+
+object ColorCommandEntry : DocEntry(
+    "/color Command",
+    "Der `/color` Command erlaubt es Spielern ihre Schriftfarbe im Chat zu ändern. Die Syntax ist `/color <color>`.",
+    "Der `/color` Command erlaubt es Spielern ihre Schriftfarbe zu ändern...",
+    "https://i.imgur.com/Zm01NI0.png",
+    "MooZiii",
+    arrayListOf("/color", "color", "colorcommand", "/color-command", "color-command")
+)
